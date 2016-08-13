7e178b97de2d358faa0433ee83a565d78d94f6f5
Matt Schwager <schwag09@gmail.com>
2016-08-13 19:02:25 -0400

    Added filesystem doc strings

--- lib/cohesion/filesystem.py
+++ lib/cohesion/filesystem.py
@@ -4,15 +4,24 @@ import os
 
 
 def get_file_contents(filename):
+    """
+    Return contents of a file
+    """
     with open(filename) as fd:
         return fd.read()
 
 
 def is_python_file(filename):
+    """
+    Return whether a file is a Python file or not
+    """
     return filename.endswith('.py')
 
 
 def recursively_get_files_from_directory(directory):
+    """
+    Return all filenames under recursively found in a directory
+    """
     return [
         os.path.join(root, filename)
         for root, directories, filenames in os.walk(directory)
@@ -21,6 +30,9 @@ def recursively_get_files_from_directory(directory):
 
 
 def recursively_get_python_files_from_directory(directory):
+    """
+    Return all Python filenames under recursively found in a directory
+    """
     return [
         filename
         for filename in recursively_get_files_from_directory(directory)
