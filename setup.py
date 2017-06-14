e074f94699792ab7c949a7b2f2a1fe90649896b9
Matt Schwager <schwag09@gmail.com>
2017-06-14 11:14:43 -0400

    Added Python 3.6 support

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     tests_require=tests_require,
     entry_points={
