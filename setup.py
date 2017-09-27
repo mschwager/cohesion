3cb83bbe2d21b19db51c05e9ceafeae0ba55c54d
Matt Schwager <schwag09@gmail.com>
2017-09-27 18:15:38 -0400

    Drop Python 3.2 support
    Python 3.2 is EOL:
    https://docs.python.org/devguide/index.html#branchstatus

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
