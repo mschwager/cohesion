549a934a3c8486a8d98c5561628ae834b1120047
Matt Schwager <schwag09@gmail.com>
2024-12-09 09:47:57 -0500

    Use importlib.metadata in __init__

--- cohesion/__init__.py
+++ cohesion/__init__.py
@@ -1,9 +1,16 @@
+from importlib.metadata import metadata
+
 from . import filesystem
 from . import module
 from . import parser
 
-__name__ = 'cohesion'
-__version__ = '1.1.0'
+m = metadata('cohesion')
+
+__name__ = m['Name']
+__version__ = m['Version']
+__description__ = m['Summary']
+__url__ = m['Home-page']
+__license__ = m['License']
 __all__ = [
     'filesystem',
     'module',
